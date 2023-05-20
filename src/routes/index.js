045e8b9daf184e215109042b0825a19ca6e566b6
ZONE7\chihm <haminhchi1808@gmail.com>
2023-05-20 16:25:54 +0700

    add permisstion and auth

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const { apiKey, permission } = require('../auth/checkAuth')
 const router = express.Router()
 
-router.use('/v1/api', require('./access'))
 // router.get('',(req,res,next)=>{
 //     return  res.status(200).json({
 //         message:"welcome",
 //     })
 // })
+//todo check apikey
+router.use(apiKey)
+//todo check permission
+router.use(permission('0000'))
+
+
+router.use('/v1/api', require('./access'))
+
 
 module.exports = router
