c26011b362a33f9283717f984abb93de1a2bfcb6
Michaelha1808 <haminhchi1808@gmail.com>
2024-03-27 16:17:08 +0700

    add write log level 0 and 1

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,50 +1,52 @@
 const AccessService = require("../services/access.service");
 
-const { CREATED, OK, SuccessRespone } = require('../core/success.respone')
+const { CREATED, OK, SuccessRespone } = require("../core/success.respone");
+const { BadRequestError } = require("../core/error.respone");
 
 class AccessController {
+  handlerRefreshToken = async (req, res, next) => {
+    // new SuccessRespone({
+    //     message: 'Get token success!',
+    //     metadata: await AccessService.handlerRefreshToken(req.body.refreshToken),
+    // }).send(res)
 
-    handlerRefreshToken = async (req, res, next) => {
-        // new SuccessRespone({
-        //     message: 'Get token success!',
-        //     metadata: await AccessService.handlerRefreshToken(req.body.refreshToken),
-        // }).send(res)
+    //* version 2 fixed, no need accessToken
+    new SuccessRespone({
+      message: "Get token success!",
+      metadata: await AccessService.handlerRefreshTokenV2({
+        refreshToken: req.refreshToken,
+        user: req.user,
+        keyStore: req.keyStore,
+      }),
+    }).send(res);
+  };
 
-        //* version 2 fixed, no need accessToken
-        new SuccessRespone({
-            message: 'Get token success!',
-            metadata: await AccessService.handlerRefreshTokenV2({
-                refreshToken: req.refreshToken,
-                user: req.user,
-                keyStore: req.keyStore
-            }),
-        }).send(res)
+  logout = async (req, res, next) => {
+    new SuccessRespone({
+      message: "Logout success!",
+      metadata: await AccessService.logout(req.keyStore),
+    }).send(res);
+  };
+
+  login = async (req, res, next) => {
+    const { email } = req.body;
+    if (!email) {
+      throw new BadRequestError("email missing");
     }
+    new SuccessRespone({
+      metadata: await AccessService.login(req.body),
+    }).send(res);
+  };
 
-    logout = async (req, res, next) => {
-        new SuccessRespone({
-            message: 'Logout success!',
-            metadata: await AccessService.logout(req.keyStore),
-        }).send(res)
-    }
-
-    login = async (req, res, next) => {
-        new SuccessRespone({
-            metadata: await AccessService.login(req.body),
-        }).send(res)
-    }
-
-    signUp = async (req, res, next) => {
-
-        new CREATED({
-            message: 'Registered OK!',
-            metadata: await AccessService.signUp(req.body),
-            options: {
-                limit: 9
-            }
-        }).send(res)
-        //  return res.status(201).json( await AccessService.signUp(req.body))
-
-    }
+  signUp = async (req, res, next) => {
+    new CREATED({
+      message: "Registered OK!",
+      metadata: await AccessService.signUp(req.body),
+      options: {
+        limit: 9,
+      },
+    }).send(res);
+    //  return res.status(201).json( await AccessService.signUp(req.body))
+  };
 }
-module.exports = new AccessController()
+module.exports = new AccessController();
--- src/core/error.respone.js
+++ src/core/error.respone.js
@@ -1,54 +1,72 @@
-const { StatusCodes, ReasonPhrases } = require("../utils/httpStatusCode")
+const logger = require("../loggers/winston.log");
+const { StatusCodes, ReasonPhrases } = require("../utils/httpStatusCode");
 
 const StatusCode = {
-    FORBIDDEN: 403,
-    CONFLICT: 409
-}
+  FORBIDDEN: 403,
+  CONFLICT: 409,
+};
 
 const ReasonStatusCode = {
-    FORBIDDEN: 'Bad request error',
-    CONFLICT: 'Conflict error'
-}
-
+  FORBIDDEN: "Bad request error",
+  CONFLICT: "Conflict error",
+};
 
 class ErrorRespone extends Error {
-    constructor(message, status) {
-        super(message)
-        this.status = status
-    }
+  constructor(message, status) {
+    super(message);
+    this.status = status;
+    //TODO
+    // Log error use winston
+    logger.error(`${this.status} - ${this.message}`);
+  }
 }
 
 class ConflictRequestError extends ErrorRespone {
-    constructor(message = ReasonStatusCode.CONFLICT, statusCode = StatusCode.FORBIDDEN) {
-        super(message, statusCode)
-    }
+  constructor(
+    message = ReasonStatusCode.CONFLICT,
+    statusCode = StatusCode.FORBIDDEN
+  ) {
+    super(message, statusCode);
+  }
 }
 
 class BadRequestError extends ErrorRespone {
-    constructor(message = ReasonStatusCode.CONFLICT, statusCode = StatusCode.FORBIDDEN) {
-        super(message, statusCode)
-    }
+  constructor(
+    message = ReasonStatusCode.CONFLICT,
+    statusCode = StatusCode.FORBIDDEN
+  ) {
+    super(message, statusCode);
+  }
 }
 
 class AuthFailureError extends ErrorRespone {
-    constructor(message = ReasonPhrases.UNAUTHORIZED, statusCode = StatusCodes.UNAUTHORIZED) {
-        super(message, statusCode)
-    }
+  constructor(
+    message = ReasonPhrases.UNAUTHORIZED,
+    statusCode = StatusCodes.UNAUTHORIZED
+  ) {
+    super(message, statusCode);
+  }
 }
 class NotFoundError extends ErrorRespone {
-    constructor(message = ReasonPhrases.NOT_FOUND, statusCode = StatusCodes.NOT_FOUND) {
-        super(message, statusCode)
-    }
+  constructor(
+    message = ReasonPhrases.NOT_FOUND,
+    statusCode = StatusCodes.NOT_FOUND
+  ) {
+    super(message, statusCode);
+  }
 }
 class ForbiddenError extends ErrorRespone {
-    constructor(message = ReasonPhrases.FORBIDDEN, statusCode = StatusCodes.FORBIDDEN) {
-        super(message, statusCode)
-    }
+  constructor(
+    message = ReasonPhrases.FORBIDDEN,
+    statusCode = StatusCodes.FORBIDDEN
+  ) {
+    super(message, statusCode);
+  }
 }
 module.exports = {
-    ConflictRequestError,
-    BadRequestError,
-    NotFoundError,
-    AuthFailureError,
-    ForbiddenError
-}
+  ConflictRequestError,
+  BadRequestError,
+  NotFoundError,
+  AuthFailureError,
+  ForbiddenError,
+};
